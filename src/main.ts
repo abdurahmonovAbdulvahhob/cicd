e712ef18056f2994cf3fa13f875a18c4900af244
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2024-10-28 10:37:15 +0500

    added log version

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
+    console.log(`listening on ${PORT}`)
   });
 }
 bootstrap();
